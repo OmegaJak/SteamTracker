2a5565e245054ac4df4ac837576b12e1e265040c
OmegaJak <jak.kruger@gmail.com>
2018-03-15 18:25:02 -0500

    Basic implementation of cost inputs

---
+++ src/CostInput.vue
@@ -0,0 +1,27 @@
+<template>
+	<input type="text" min="0" v-model="cost" @keypress="filterInputs($event)" @blur="formatDollars">
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+			cost: 7.50,
+		};
+	},
+	methods: {
+		filterInputs(event) {
+			let validNum = /^(\d*\.)?\d*$/.test(event.target.value + event.key);
+			if (!validNum) {
+				event.preventDefault();
+			}
+		},
+		formatDollars() {
+      if (this.cost !== "") {
+				let num = Number(this.cost);
+
+				this.cost = num.toFixed(2);
+      }
+	},
+};
+</script>
