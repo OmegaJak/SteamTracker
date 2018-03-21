e079ffef0caf388aa31aaf3621064a1807d9ac1d
OmegaJak <jak.kruger@gmail.com>
2018-03-21 00:01:34 -0500

    Refined costPerHr implementation

---
+++ src/CostPerHr.vue
@@ -0,0 +1,23 @@
+<template>
+	<div> {{ format(this.costPerHr) }} </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+			displayValue: this.format(this.costPerHr),
+		};
+	},
+	props: ["costPerHr"],
+	methods: {
+		format(val) {
+			if (val === String("-")) {
+        return "-";
+      } else {
+        return val.toFixed(2);
+      }
+		},
+	},
+}
+</script>
