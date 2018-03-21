9f0085cbe91ce02a5e3aed26f60ed922d61f78d2
OmegaJak <jak.kruger@gmail.com>
2018-03-20 21:40:45 -0500

    Handled cost input edge cases
    Specifically, input like ".", ".002"
    Also accounted for NaN

--- src/SteamTrackerLib.ts
+++ src/SteamTrackerLib.ts
@@ -5,12 +5,20 @@
  * @param cost A string representing the numerical cost to be formatted
  */
 export function formatDollars(cost: string) {
-	if (cost !== "" && cost !== undefined) {
+	if (cost === "" || cost === undefined || cost === String(".") || cost === String("NaN")) {
+		return "";
+	} else {
 		let num = Number(cost); // Converting to Number removes trailing zeros
+		if (isNaN(num)) {
+			console.error("formatDollars encountered an unexpected input of \"" + cost + "\".");
+			return "";
+		}
+
 		let parts = String(num).split(".");
 
 		if (parts.length === 1 || parts[1].length < 2)
 			return num.toFixed(2);
+
+		return parts[0] + (parts.length === 2 ? ("." + parts[1]) : "");
 	}
-	return cost;
 }
