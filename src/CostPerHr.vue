02386dcf64e0db84bb54b6a8ff4f5dbe57478c35
OmegaJak <jak.kruger@gmail.com>
2018-04-30 23:17:56 -0500

    Reworked Cost/Hr formatting

--- src/CostPerHr.vue
+++ src/CostPerHr.vue
@@ -10,14 +10,31 @@ export default {
 			if (val === String("-")) {
         return "-";
       } else {
-        return val.toFixed(2);
+				let decimal = val - Math.floor(val); // Remove integer portion
+
+				let intStr = String(val.toFixed(1)).split(".")[0]; // Get the integer portion
+				let decimalStr: string;
+				if (intStr.length > 1) {
+					decimalStr = decimal.toFixed(2); // If we have at least 2 digits before ".", only show two digits after "."
+				} else {
+					decimalStr = decimal.toPrecision(3); // Otherwise, round based on sig figs
+				}
+				if (decimalStr.length > 5) { // May have gotten too many digits to the right of "."
+					if (val < 1) {
+						decimalStr = decimal.toFixed(4); // So if it's less than 1, reduce to 4 digits after "."
+					} else {
+						decimalStr = decimal.toFixed(3); // If it's over one, only do 3 digits after "."
+					}
+				}
+
+				return intStr + decimalStr.substring(1); // The "." comes with the decimalStr. Substring to remove 0 before "."
       }
 		},
 		getCostPerHr(cost: string, playtimeMinutes: number) {
 			let costAsNum = (cost === "" || cost === undefined) ? 0 : Number(cost);
 			if (playtimeMinutes === 0)
 				return "-";
-			return costAsNum / (playtimeMinutes / 60); // Rounds to 3 decimals (sorta poorly)
+			return costAsNum / (playtimeMinutes / 60);
 		},
 	},
 };
